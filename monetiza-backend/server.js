2a5391031b94708ae1d827d4589a5e6c3a0df0e1
Gabriel Carvalho <gabrielivanildo2009@gmail.com>
2026-06-10 10:19:49 -0300

    Third Commit

--- monetiza-backend/server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./src/app')
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
